00efd802884359b7d82abab2da6f30e49b4995ad
raj073 <rajoni.das@outlook.com>
2022-11-25 01:03:22 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,14 @@ const app = express();
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rcsgtvg.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('/', async(req, res) => {
     res.send("Car Cruise BD Server is Running");
