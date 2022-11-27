91c259982a673afadbc2ef5bf851658744d91b53
raj073 <rajoni.das@outlook.com>
2022-11-28 00:18:48 +0600

    Delete api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -111,6 +111,14 @@ async function run(){
             const result = await usersCollection.insertOne(users);
             res.send(result);
         });
+
+        //Product Delete API
+        app.delete('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: ObjectId(id)};
+        const result = await productsCollection.deleteOne(filter);
+        res.send(result);
+        })
 
 
 
